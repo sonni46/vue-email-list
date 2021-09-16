f8a87feb60d2a7ba002735a8cf1a4f33f73eb593
Marco <sonni46marcosenpai@gmail.com>
2021-09-16 15:13:39 +0200

    update

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ Vue.config.devtools = true;
 const app = new Vue ({
     el: "#root",
     data: {
-
+        counter : 10,
+        mess: [],
     },
     mounted () {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((resp)=>{
-
-        })
+            for(let i = 0; i < this.counter; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp)=>{
+                //    console.log(i +  resp.data.response);
+                this.mess.push( i +  resp.data.response)
+                })
+            }
     }
 })
